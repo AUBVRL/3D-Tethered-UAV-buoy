464987f3bc7f85e591c2d3b5b7e699910db8f752
Ahmad Kourani <kourani.ahmad@gmail.com>
2021-10-16 11:58:04 +0300

    combine filters declarations into struct.

--- src/estimation.m
+++ src/estimation.m
@@ -24,59 +24,61 @@ X_GPS_offset = [0.01 0.005];  % GPS offset error from true CG. [m]
 % an3=[0.420807779837732,0.806706851924986,0.420807779837732];
 % bn3=[1,0.806706851924986,-0.158384440324536];
 
-%
-an1=[0.337540151883547,-0.271994199011296,0.337540151883547];
-bn1=[1,-0.271994199011296,-0.324919696232906];
-an2=[0.327054913984048,0.404148045226953,0.327054913984048];
-bn2=[1,0.404148045226953,-0.345890172031904];
-an3=[0.420807779837732,0.806706851924986,0.420807779837732];
-bn3=[1,0.806706851924986,-0.158384440324536];
-
-an1_v=[0.4208 -0.8097 0.4208];
-bn1_v=[1 -0.8097 -0.1584];
-
-
-% Resample notchs for Ts ~= 1/500 s:
-
-temp = d2d(tf(an1,bn1,1/500),Tn);
-an1 = temp.numerator{1};
-bn1 = temp.denominator{1};
-%
-temp = d2d(tf(an2,bn2,1/500),Tn);
-an2 = temp.numerator{1};
-bn2 = temp.denominator{1};
-%
-temp = d2d(tf(an3,bn3,1/500),Tn);
-an3 = temp.numerator{1};
-bn3 = temp.denominator{1};
-%
-temp = d2d(tf(an1_v,bn1_v,1/500),Tn);
-an1_v = temp.numerator{1};
-bn1_v = temp.denominator{1};
+%% 
+% % an1=[0.337540151883547,-0.271994199011296,0.337540151883547];
+% % bn1=[1,-0.271994199011296,-0.324919696232906];
+% % an2=[0.327054913984048,0.404148045226953,0.327054913984048];
+% % bn2=[1,0.404148045226953,-0.345890172031904];
+% % an3=[0.420807779837732,0.806706851924986,0.420807779837732];
+% % bn3=[1,0.806706851924986,-0.158384440324536];
+% % 
+% % an1_v=[0.4208 -0.8097 0.4208];
+% % bn1_v=[1 -0.8097 -0.1584];
+% % 
+% % 
+% % % Resample notchs for Ts ~= 1/500 s:
+% % 
+% % temp = d2d(tf(an1,bn1,1/500),Tn);
+% % an1 = temp.numerator{1};
+% % bn1 = temp.denominator{1};
+% % %
+% % temp = d2d(tf(an2,bn2,1/500),Tn);
+% % an2 = temp.numerator{1};
+% % bn2 = temp.denominator{1};
+% % %
+% % temp = d2d(tf(an3,bn3,1/500),Tn);
+% % an3 = temp.numerator{1};
+% % bn3 = temp.denominator{1};
+% % %
+% % temp = d2d(tf(an1_v,bn1_v,1/500),Tn);
+% % an1_v = temp.numerator{1};
+% % bn1_v = temp.denominator{1};
 
 % c2d:
 
 %motors discrete transfer function:
 sys=tf(1,[Tm 1]);
 sysd = c2d(sys,Ts);
-Am = sysd.denominator{1};
-Bm = sysd.numerator{1};
+filters.Am = sysd.denominator{1};
+filters.Bm = sysd.numerator{1};
 
 % High pass filters:
 sys=tf([OMEGA_FILTER 0],[OMEGA_FILTER 1]);
 sysd = c2d(sys,Ts);
-Am_HPF = sysd.denominator{1};
-Bm_HPF = sysd.numerator{1};
+filters.Am_HPF = sysd.denominator{1};
+filters.Bm_HPF = sysd.numerator{1};
 
 % Low pass filter:
 sys=tf([OMEGA_FILTER],[1 OMEGA_FILTER]);
 sysd = c2d(sys,Ts);
-Am_LPF = sysd.denominator{1};
-Bm_LPF = sysd.numerator{1};
+filters.Am_LPF = sysd.denominator{1};
+filters.Bm_LPF = sysd.numerator{1};
 
 OMEGA_FILTER2 = 15;    % (rad/s) - For smoothing optitrack x and z data
 % Low pass filter:
 sys=tf([OMEGA_FILTER2],[1 OMEGA_FILTER2]);
 sysd = c2d(sys,Ts);
-Am_LPF2 = sysd.denominator{1};
-Bm_LPF2 = sysd.numerator{1};
+filters.Am_LPF2 = sysd.denominator{1};
+filters.Bm_LPF2 = sysd.numerator{1};
+
+clear sys sysd
